f02831aadeabaac1e59d67ee90826d935992c5e7
Saúl Santana <sausantana@aidacanarias.com>
2023-10-30 12:34:08 +0000

    quick fix

--- app/src/main/java/com/sss/flightsearch/ui/screens/flight_screen/FlightResults.kt
+++ app/src/main/java/com/sss/flightsearch/ui/screens/flight_screen/FlightResults.kt
@@ -7,8 +7,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.lixoten.flightsearch.ui.screens.flight_screen.FlightRow
-import com.sss.flightsearch.ui.screens.flight_screen.FlightRow
 import com.sss.flightsearch.data.MockData
 import com.sss.flightsearch.model.Airport
 import com.sss.flightsearch.model.Favorite
